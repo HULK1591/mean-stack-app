a2f2b78c61b07a61047709f7b763580b892cb75b
Shyam Patel <Sam_r_patel@hotmail.com>
2019-03-11 16:33:52 -0400

    try out some new ports

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Connected");
+var express = require('express');
+var app = express();
+
+//SET the port num that the application is going to listern to
+app.set('port', 3000);
+
+app.listen(app.get('port'), () =>{
+    console.log('Move bitch! Express running at port: ' + app.get('port'));
+});
