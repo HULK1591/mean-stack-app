29e2cade9b6efe8b686b612cf28a0b8fb317bf7e
Shyam Patel <Sam_r_patel@hotmail.com>
2019-03-14 16:14:18 -0400

    abc

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var app = express();
 //SET the port num that the application is going to listern to
 app.set('port', 3000);
 
-app.listen(app.get('port'), () =>{
+var server = app.listen(app.get('port'), () =>{
+    var port = server.address().port;
     console.log('Move bitch! Express running at port: ' + app.get('port'));
 });
+console.log('Me first');
